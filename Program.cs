e8c78854e883c24fb571fb1ab2f9d2cd09ed4bbe
IvaFil <107261208+IvaFil@users.noreply.github.com>
2022-08-31 17:24:21 +0300

    Разбиение массива

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace йоу
+{
+    internal class Program
+    {
+     
+
+        static void Main(string[] args)
+        {
+
+           
+
+                Console.WriteLine("Введи число: ");
+
+            int a = int.Parse(Console.ReadLine());
+
+            string str =a.ToString();
+
+            int[]myArray=new int[str.Length];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                myArray[i] = int.Parse(str[i]);
+            }
+
+            Console.WriteLine(myArray[2]);
+
+
+
+
+
+
+          
+                
+
+         
+
+            
+
+        }
+    }
+}
